2d3c3822e0275b8c7bc470d2d8011960cd3d6eaf
Александр Нейчев <alexnejchev73@gmail.com>
2019-06-27 15:14:27 +0400

    Added test project

---
+++ UnitTestSurvey/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestSurvey")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestSurvey")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("12b0ad53-73d7-4b5d-a356-04aca73d6686")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestSurvey/UnitTestDB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestSurvey
+{
+    [TestClass]
+    public class UnitTestDB
+    {
+        [TestMethod]
+        public void InitDB()
+        {
+
+        }
+    }
+}
