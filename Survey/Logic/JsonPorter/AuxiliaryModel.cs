7e15f7281949e33b80f8d441b6b08ec9f21a9ecc
Dmitriy <Dva-1999@yandex.ru>
2019-06-29 15:06:40 +0400

    Prepared the environment

---
+++ Survey/Logic/JsonPorter/AuxiliaryModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace JsonHandler
+{
+
+    [DataContract]
+    public class ListOfStuff//JsonType1
+    {
+        public ListOfStuff()
+        {
+
+        }
+        [DataMember] public int id { get; set; }
+        [DataMember] public string result { get; set; }
+    }
+
+    [DataContract]
+    public class ListOfSurvey//JsonType2
+    {
+        public ListOfSurvey()
+        {
+
+        }
+        [DataMember] public string name_of_survey { get; set; }
+        [DataMember] public List<ListOfStuff> list_of_stuff { get; set; }
+    }
+
+    public class AuxiliaryModel
+    {
+        public AuxiliaryModel()
+        {
+
+        }
+        public string category { get; set; }
+        public List<ListOfSurvey> list_of_survey { get; set; }
+        //public List<ListOfStuff> list_of_stuff { get; set; }
+    }
+}
