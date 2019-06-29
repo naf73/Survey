16a5ed3c23d7910d9448098633f3aec594585c5f
bobryshev_m.v@mail.ru <bobryshev_m.v@mail.ru>
2019-06-29 16:46:30 +0400

    AddLANG

---
+++ Survey/Helper/LANG.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Survey.Helper
+{
+
+    public enum Lang
+    {
+        RUS,
+        ENG,
+    }
+    class LANG
+    {
+        public static string l1 { get; private set; }
+        public static string l2 { get; private set; }
+
+
+
+
+        public static void ChangeLang(Lang lang)
+        {
+            switch (lang)
+            {
+                case Lang.RUS:
+                    l1 = "дом";
+                    l2 = "имя";
+                    break;
+                case Lang.ENG:
+                    l1 = "home";
+                    l2 = "name";
+                    break;
+                default:
+                    break;
+            }
+        }
+}
---
+++ Survey/Helper/LangPages.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Survey.Helper
+{
+    public static class LangPages
+    {
+        public static class AdminPage
+        {
+            public static string DgLastName { get; private set; }
+            public static string DgName { get; private set; }
+            public static string DgRating { get; private set; }
+            public static string DgTest { get; private set; }
+            public static string DgCategory { get; private set; }
+            public static string DgTotal { get; private set; }
+            public static string DgStatiatic { get; private set; }
+            public static string KcWorkers { get; private set; }
+            public static string KcSurveys { get; private set; }
+            public static string KcExit { get; private set; }
+
+
+        }
+        public static class CategoriesPage
+        {
+            public static string DgCatTests { get; private set; }            
+            public static string KcNewCat { get; private set; }
+            public static string KcChangeCat { get; private set; }
+            public static string KcDelCat { get; private set; }
+            public static string KcLookTests { get; private set; }
+            public static string KcExit { get; private set; }
+
+
+
+        }
+    }
+}
