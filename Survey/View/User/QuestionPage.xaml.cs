0105c9214d5ef8c9d32471cc46774a7f608355df
Александр Нейчев <alexnejchev73@gmail.com>
2019-07-05 09:26:24 +0400

    Manual merge

--- Survey/View/User/QuestionPage.xaml.cs
+++ Survey/View/User/QuestionPage.xaml.cs
@@ -79,14 +79,9 @@ namespace Survey.View.User
             {
                 if (!CheckGetUserAnswer())
                 {
-                    MessageBox.Show("Выберите ответ");
+                    MessageBox.Show(LangPages.MBox.ChooseAnsw);
                     return;
                 }
-            if (!CheckGetUserAnswer())
-            {
-                MessageBox.Show(LangPages.MBox.ChooseAnsw);
-                return;
-            }
 
                 if (CheckAnswerCount() && CheckIsRightUserAnswer())
                 {
